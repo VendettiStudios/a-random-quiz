d9432525e94e3317fe4d29b8b06230bd18662222
DH <daniel@vendettistudios.com>
2022-05-22 13:30:04 -0700

    HTML Filled with comments, added JS Questions Array, beginning simple css design

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+// question array
+let questions = [
+    {
+      title: "How many letters are in Four",
+      choices: ["1", "2", "3", "4"],
+      answer: "4"
+    },
+    {
+      title: "How many vowels in five",
+      choices: ["1", "2", "3", "4"],
+      answer: "2"
+    },
+    {
+      title: "What movie has a wizard",
+      choices: ["Die Hard", "The Dark Knight", "Beauty and The Beast", "Harry Potter"],
+      answer: "Harry Potter"
+    },
+    {
+      title: "What is the capital of America",
+      choices: ["Rio", "England", "Washington DC", "Washington state"],
+      answer: "Washington DC"
+    },
+    {
+      title: "Which fast-food company piloted a chicken-flavored nail polish?",
+      choices: ["Raisin Canes", "Zaxbys", "Mcdonalds", "KFC"],
+      answer: "KFC"
+    }
+  ];
